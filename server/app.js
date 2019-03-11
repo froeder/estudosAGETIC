4dca3c7dfc2a78d5edec41bfbf05d86ed1373489
Jhonatan Froeder <jhonatan.froeder@aluno.ufms.br>
2019-03-11 07:49:11 -0400

    Inclusao verificação login

--- server/app.js
+++ server/app.js
@@ -1,41 +1,58 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const uuid = require("uuid");
-const port = 3001
+const port = 3001;
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
 
-var mysql = require('mysql')
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'testes'
-})
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "testes"
+});
 
-connection.connect()
+connection.connect();
 
-app.get('/', function (req, res) {
-    res.send('Hellor Word');
-    console.log(req)
-})
+app.get("/", function (req, res) {
+    res.send("Hellor Word");
+    console.log(req);
+});
 
-app.post('/adicionar-usuario', function (req, res) {
-    var id = uuid()
-    var nome = req.body.nome
-    var email = req.body.email
-    var senha = req.body.senha
-    var data_nascimento = req.body.data_nascimento
-    var instituicao = req.body.instituicao
-    var profissao = req.body.profissao
+app.post("/adicionar-usuario", function (req, res) {
+    var id = uuid();
+    var nome = req.body.nome;
+    var email = req.body.email;
+    var senha = req.body.senha;
+    var data_nascimento = req.body.data_nascimento;
+    var instituicao = req.body.instituicao;
+    var profissao = req.body.profissao;
 
-    connection.query("INSERT INTO `usuarios`(`id`,  `nome`,`email`, `senha`, `data_nascimento`, `instituicao`, `profissao`) VALUES ('" + id + "','" + nome + "','" + email + "','" + senha + "','" + data_nascimento + "','" + instituicao + "','" + profissao + "')",
+    connection.query(
+        "INSERT INTO `usuarios`(`id`,  `nome`,`email`, `senha`, `data_nascimento`, `instituicao`, `profissao`) VALUES ('" +
+        id +
+        "','" +
+        nome +
+        "','" +
+        email +
+        "','" +
+        senha +
+        "','" +
+        data_nascimento +
+        "','" +
+        instituicao +
+        "','" +
+        profissao +
+        "')",
         function (err, result) {
             if (err) throw err;
             console.log([{
@@ -44,43 +61,49 @@ app.post('/adicionar-usuario', function (req, res) {
                 email: email,
                 senha: senha,
                 data_nascimento: data_nascimento
-            }])
-            res.send('Dado adicionado com sucesso');
+            }]);
+            res.send("Dado adicionado com sucesso");
         }
-    )
-})
+    );
+});
 
-app.get('/exibir-usuarios', function (req, res) {
-    connection.query('SELECT * FROM usuarios', function (err, rows, fields) {
-        if (err) throw err
-        console.log(rows)
-        res.send(rows)
-    })
-})
+app.get("/exibir-usuarios", function (req, res) {
+    connection.query("SELECT * FROM usuarios", function (err, rows, fields) {
+        if (err) throw err;
+        console.log(rows);
+        res.send(rows);
+    });
+});
 
-app.get('/visualizar-usuario', function (req, res) {
-    console.log('Visualizar usuário')
-})
+app.get("/visualizar-usuario", function (req, res) {
+    console.log("Visualizar usuário");
+});
 
-app.put('/atualizar-usuario', function (req, res) {
-    console.log('Atualizar usuário')
-})
+app.put("/atualizar-usuario", function (req, res) {
+    console.log("Atualizar usuário");
+});
 
-app.delete('/deletar-usuario', function (req, res) {
-    console.log('Deletar usuário')
-})
+app.delete("/deletar-usuario", function (req, res) {
+    console.log("Deletar usuário");
+});
 
-app.post('/login', function (req, res) {
-    var email = req.body.email
-    var senha = req.body.senha
+app.post("/login", function (req, res) {
+    var email = req.body.email;
+    var senha = req.body.senha;
 
-    connection.query("SELECT * FROM usuarios WHERE email = '" + email + "'", function (err, rows, fields) {
-        if (err) throw err
-        console.log(rows)
+    connection.query(
+        "SELECT * FROM usuarios WHERE email = '" + email + "'",
+        function (err, rows, fields) {
+            if (err) {
+                throw err;
+                res.send(err)
+            }
+            console.log(rows);
 
-    })
+            res.send(rows)
+        }
+    );
 
-    res.send('LOGIN')
-})
+});
 
 app.listen(port, () => console.log(`Server runing on port ${port}`));
---
+++ teste/src/pages/auth/index.js
@@ -0,0 +1,30 @@
+function confereSenha(senha_cliente, senha_servidor) {
+    if (senha_cliente == senha_servidor) {
+        console.log('Igual')
+    } else {
+        console.log('Diferentes')
+        alert('Senha Incorreta')
+    }
+}
+
+function login() {
+    let email = $("#email").val();
+    let senha = $("#senha").val();
+
+    axios.post("http://127.0.0.1:3001/login", {
+            email: email,
+            senha: senha
+        }).then(function (response) {
+            let dados = response.data[0]
+
+            console.log(dados)
+            this.confereSenha(senha, dados.senha)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function cadastrar() {
+    window.location.replace('cadastrar_usuario.html')
+}
